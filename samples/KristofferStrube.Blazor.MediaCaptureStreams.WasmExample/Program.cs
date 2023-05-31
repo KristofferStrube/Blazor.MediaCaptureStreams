e7d8d5ebffdfdd2b99eae1700da72e1c9f615fc5
KristofferStrube <kristoffer.strube@gmail.com>
2023-05-31 16:44:36 +0200

    Restructure examples.

--- samples/KristofferStrube.Blazor.MediaCaptureStreams.WasmExample/Program.cs
+++ samples/KristofferStrube.Blazor.MediaCaptureStreams.WasmExample/Program.cs
@@ -10,4 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMediaDevicesService();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+await app.RunAsync();
