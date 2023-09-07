0f517dc0f1c97462311c2eb72ff9af25c592756e
KristofferStrube <kristoffer.strube@gmail.com>
2023-09-07 22:06:41 +0200

    Started on adding Playwright test setup.

---
+++ tests/IntegrationTests/MediaDevicesTest.cs
@@ -0,0 +1,14 @@
+﻿namespace KristofferStrube.Blazor.MediaCaptureStreams.IntegrationTests;
+
+public class MediaDevicesTest : MediaBlazorTest
+{
+    [Test]
+    public async Task GetUserMedia_ReturnsAudioMediaStream_WhenAccepting()
+    {
+        // Act
+        await Page.GotoAsync(RootUri.AbsoluteUri + "/MediaDevices/GetUserMedia/ReturnsAudioMediaStream");
+
+        // Assert
+        await Expect(Page.GetByTestId("result")).ToHaveTextAsync("success");
+    }
+}
---
+++ tests/IntegrationTests/MediaDevicesTestNoPermission.cs
@@ -0,0 +1,16 @@
+﻿namespace KristofferStrube.Blazor.MediaCaptureStreams.IntegrationTests;
+
+public class MediaDevicesTestNoPermission : MediaBlazorTest
+{
+    protected override string[] Args => Array.Empty<string>(); 
+
+    [Test]
+    public async Task GetUserMedia_ThrowsException_WhenDismissing()
+    {
+        // Act
+        await Page.GotoAsync(RootUri.AbsoluteUri + "/MediaDevices/GetUserMedia/ReturnsAudioMediaStream");
+
+        // Assert
+        await Expect(Page.GetByTestId("result")).ToHaveTextAsync("Not supported");
+    }
+}
